154439963571f0b8b56285a600a9c44703c58f5c
Noel Manuel <setsuna04@gmail.com>
2018-02-08 00:00:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {'Keiko Matsui': 'The Sun Rises'}
 function updatePlaylist(obj, artist, song) {
   obj[artist] = song;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
